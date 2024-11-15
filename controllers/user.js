2bb5f500cda4f14df4e512028cbe92c52c99f6ce
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-15 16:38:42 +0700

    create login feature

--- controllers/user.js
+++ controllers/user.js
@@ -2,7 +2,7 @@ import { BadRequestError } from "../errors/BadRequestError.js";
 import { UnauthorizedError } from "../errors/UnauthorizedError.js";
 import bcrypt from "bcryptjs";
 import pool from "../db/db.js";
-import { text } from "express";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
   const { password, noHp, alamat, email, kelId } = req.body;
@@ -23,5 +23,30 @@ export const register = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  return res.json("login");
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    throw new BadRequestError("All specified field must be included");
+  }
+
+  const textQuery = `SELECT pengguna_id, password, email, role FROM Pengguna WHERE email = $1`;
+
+  const queryResult = await pool.query(textQuery, [email]);
+
+  if (queryResult.rowCount === 0) {
+    throw new UnauthorizedError("Email is not registered");
+  }
+
+  const user = queryResult.rows[0];
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatch) {
+    throw new UnauthorizedError("Incorrect password");
+  }
+
+  const token = jwt.sign({ pengguna_id: user.pengguna_id, role: user.role }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+
+  return res.status(200).json({ success: true, token });
 };
