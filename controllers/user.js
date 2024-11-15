31ffdb611f0facaabe7fdd091fb3a69fd368aa6c
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-15 16:21:16 +0700

    create register feature

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,22 @@
+import { BadRequestError } from "../errors/BadRequestError.js";
+import bcrypt from "bcryptjs";
+import pool from "../db/db.js";
+import { text } from "express";
+
 export const register = async (req, res) => {
-  return res.json("register");
+  const { password, noHp, alamat, email, kelId } = req.body;
+
+  if (!password || !noHp || !alamat || !email || !kelId) {
+    throw new BadRequestError("All specified field must be included");
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const textQuery = `INSERT INTO Pengguna (no_hp, alamat, email, kel_id, password) VALUES ($1, $2, $3, $4, $5)`;
+  const values = [noHp, alamat, email, kelId, hashedPassword];
+
+  await pool.query(textQuery, values);
+
+  return res.status(200).json({ success: true });
 };
