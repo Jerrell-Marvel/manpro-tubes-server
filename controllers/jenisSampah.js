088376bab164c8d1a24be97e07199324bff33933
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-15 18:02:29 +0700

    create add jenis sampah feature

--- controllers/jenisSampah.js
+++ controllers/jenisSampah.js
@@ -1,4 +1,5 @@
 import pool from "../db/db.js";
+import { BadRequestError } from "../errors/BadRequestError.js";
 
 export const getJenisSampah = async (req, res) => {
   const queryText = `SELECT * FROM jenis_sampah`;
@@ -8,7 +9,20 @@ export const getJenisSampah = async (req, res) => {
 };
 
 export const createJenisSampah = async (req, res) => {
-  return res.json("create jenis sampah");
+  const { namaJenisSampah } = req.body;
+
+  if (!namaJenisSampah) {
+    throw new BadRequestError("All specified field must be included");
+  }
+
+  const queryText = `INSERT INTO Jenis_Sampah (nama_jenis_sampah) VALUES ($1) RETURNING jenis_sampah_id`;
+  const values = [namaJenisSampah];
+
+  const queryResult = await pool.query(queryText, values);
+
+  const jenisSampahId = queryResult.rows[0].jenis_sampah_id;
+
+  return res.json({ success: true, jenisSampahId });
 };
 
 export const updateJenisSampah = async (req, res) => {
