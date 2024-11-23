c93b4c21c1ae9f4a8cbf1e1802d2b3ff57165a80
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-23 11:26:45 +0700

    create transaksi keluar route

---
+++ controllers/transaksiKeluar.js
@@ -0,0 +1,7 @@
+export const createTransaksiKeluar = async (req, res) => {
+  return res.json("create transaksi keluar");
+};
+
+export const getTransaksiKeluar = async (req, res) => {
+  return res.json("get all transaksi keluar");
+};
