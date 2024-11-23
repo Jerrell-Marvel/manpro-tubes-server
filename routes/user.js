48acbec618291a4967a4cb66f756a7e6e24f52ef
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-23 20:33:21 +0700

    protect get all users and update user route

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { register, login, getAllUsers, updateUser } from "../controllers/user.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get("/users", getAllUsers);
-router.patch("/users/:penggunaId", updateUser);
+router.get("/users", authMiddleware("admin"), getAllUsers);
+router.patch("/users/:penggunaId", authMiddleware("admin"), updateUser);
 router.post("/register", register);
 router.post("/login", login);
 
