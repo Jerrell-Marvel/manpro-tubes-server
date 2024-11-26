293c32e4d387cb3e6e2c8309f2eb25769d375db7
Jerrell Marvel <jerrell.marvel88@gmail.com>
2024-11-26 09:57:17 +0700

    fix inventory response to shownama suk

--- controllers/inventory.js
+++ controllers/inventory.js
@@ -1,7 +1,7 @@
 import pool from "../db/db.js";
 
 export const getAllInventory = async (req, res) => {
-  const queryText = "SELECT * FROM Inventory_Sampah i INNER JOIN Sampah s ON i.sampah_id = s.sampah_id INNER JOIN Harga h ON  s.harga_id_sekarang = h.harga_id";
+  const queryText = "SELECT * FROM Inventory_Sampah i INNER JOIN Sampah s ON i.sampah_id = s.sampah_id INNER JOIN Harga h ON  s.harga_id_sekarang = h.harga_id INNER JOIN SUK su ON s.suk_id=su.suk_id";
 
   const queryResult = await pool.query(queryText);
 
